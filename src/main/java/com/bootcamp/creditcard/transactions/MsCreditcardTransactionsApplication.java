7e01eabf6dd59ca8cbd5b56065cfe1824c222361
Renato Alonso Castillo Pingo <rcastipi@emeal.nttdata.com>
2022-03-11 18:20:20 -0500

    first commit

---
+++ src/main/java/com/bootcamp/creditcard/transactions/MsCreditcardTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.creditcard.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCreditcardTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCreditcardTransactionsApplication.class, args);
+	}
+
+}
