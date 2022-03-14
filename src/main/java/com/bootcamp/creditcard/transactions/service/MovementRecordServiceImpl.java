566e7933b7fca9b269d32b9a00c2ee83395e7adc
renato-castillo <renatocastillopingo@gmail.com>
2022-03-14 14:36:26 -0500

    Edited for bootstrap.yml

--- src/main/java/com/bootcamp/creditcard/transactions/service/MovementRecordServiceImpl.java
+++ src/main/java/com/bootcamp/creditcard/transactions/service/MovementRecordServiceImpl.java
@@ -1,6 +1,7 @@
 package com.bootcamp.creditcard.transactions.service;
 
 import com.bootcamp.creditcard.transactions.entity.MovementRecord;
+import com.bootcamp.creditcard.transactions.repository.IMovementRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -10,25 +11,25 @@ import reactor.core.publisher.Mono;
 public class MovementRecordServiceImpl implements IMovementRecordService {
 
     @Autowired
-    private IMovementRecordService recordService;
+    private IMovementRecordRepository movementRecordRepository;
 
     @Override
     public Mono<MovementRecord> save(MovementRecord movementRecord) {
-        return recordService.save(movementRecord);
+        return movementRecordRepository.save(movementRecord);
     }
 
     @Override
     public Mono<Void> deleteById(String id) {
-        return recordService.deleteById(id);
+        return movementRecordRepository.deleteById(id);
     }
 
     @Override
     public Mono<MovementRecord> findById(String id) {
-        return recordService.findById(id);
+        return movementRecordRepository.findById(id);
     }
 
     @Override
     public Flux<MovementRecord> findAll() {
-        return recordService.findAll();
+        return movementRecordRepository.findAll();
     }
 }
