247a6d8e5d7f02f8aa445dded05f34a01c52d164
renato-castillo <renatocastillopingo@gmail.com>
2022-03-16 13:30:40 -0500

    Added eureka client

--- src/main/java/com/bootcamp/creditcard/transactions/MsCreditcardTransactionsApplication.java
+++ src/main/java/com/bootcamp/creditcard/transactions/MsCreditcardTransactionsApplication.java
@@ -2,8 +2,10 @@ package com.bootcamp.creditcard.transactions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MsCreditcardTransactionsApplication {
 
 	public static void main(String[] args) {
